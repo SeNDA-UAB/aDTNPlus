55f8d2a2a284cf71ebdf00e58cff40cd014b99fe
Blackcatn13 <mdd_13@hotmail.com>
2015-06-17 15:09:57 +0200

    Adds the TimestampManager skeleton.

---
+++ BundleAgent/Utils/TimestampManager.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE TimestampManager.cpp
+ * AUTHOR Blackcatn13
+ * DATE Jun 16, 2015
+ * VERSION 1
+ * This file contains the implementation of the TimestampManager class.
+ */
+
+#include "Utils/TimestampManager.h"
+#include <utility>
+
+TimestampManager *TimestampManager::m_instance = 0;
+
+
+TimestampManager::TimestampManager()
+    : m_lastTimestamp(0),
+      m_lastSeqNum(0) {
+}
+
+TimestampManager::~TimestampManager() {
+  delete m_instance;
+}
+
+TimestampManager* TimestampManager::getInstance() {
+  if (m_instance == 0) {
+    m_instance = new TimestampManager();
+  }
+  return m_instance;
+}
+
+std::pair<uint64_t, uint64_t> TimestampManager::getTimestamp() {
+}
+
