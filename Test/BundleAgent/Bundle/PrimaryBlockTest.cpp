b9d9215e3ac46e42b99ae6eb25754171c63d6bd2
Blackcatn13 <mdd_13@hotmail.com>
2015-06-18 16:25:31 +0200

    Adds missing tests files.

---
+++ Test/BundleAgent/Bundle/PrimaryBlockTest.cpp
@@ -0,0 +1,26 @@
+/*
+* Copyright (c) 2015 SeNDA
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+/**
+ * FILE PrimaryBlockTest.cpp
+ * AUTHOR Blackcatn13
+ * DATE Jun 18, 2015
+ * VERSION 1
+ * This file contains the test of the PrimaryBlock class.
+ */
+
+#include "Bundle/PrimaryBlock.h"
+#include "gtest/gtest.h"
