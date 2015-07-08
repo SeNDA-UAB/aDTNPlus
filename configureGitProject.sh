4cec4f42dcbd6933b60705adc8177cacbb9a6706
Blackcatn13 <mdd_13@hotmail.com>
2015-07-08 10:21:24 +0200

    Adds hooks to check unit tests before pushing.

---
+++ configureGitProject.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Configure symbolic link for the hooks
+
+ln -s -f ../../hooks/pre-push .git/hooks/pre-push
--- generateTestAndCoverage.sh
+++ generateTestAndCoverage.sh
@@ -9,6 +9,7 @@ cd build
 cmake ..
 make
 ./BundleAgent_test
+RET=$?
 lcov --quiet -c -i -d . -o .coverage.base
 lcov --quiet -c -d . -o .coverage.run
 lcov --quiet --remove .coverage.base "*gtest*" -o .coverage.base
@@ -18,3 +19,4 @@ lcov --quiet --remove .coverage.run "/usr*" -o .coverage.run
 lcov --quiet -d . -a .coverage.base -a .coverage.run -o .coverage.total
 genhtml --quiet --no-branch-coverage -o coverage_html .coverage.total
 rm -f .coverage.base .coverage.run .coverage.total
+exit $RET
