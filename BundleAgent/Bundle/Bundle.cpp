2c49451fe5a427735744483f61095b6df3d73566
Blackcatn13 <mdd_13@hotmail.com>
2015-11-12 10:17:19 +0100

    Fixes Bundle getId implementation.

--- BundleAgent/Bundle/Bundle.cpp
+++ BundleAgent/Bundle/Bundle.cpp
@@ -170,6 +170,8 @@ void Bundle::addBlock(std::shared_ptr<CanonicalBlock> newBlock) {
 }
 
 std::string Bundle::getId() {
-  return m_primaryBlock->getSource() + m_primaryBlock->getCreationTimestamp()
-      + m_primaryBlock->getCreationTimestampSeqNumber();
+  std::stringstream ss;
+  ss << m_primaryBlock->getSource() << m_primaryBlock->getCreationTimestamp()
+     << m_primaryBlock->getCreationTimestampSeqNumber();
+  return ss.str();
 }
