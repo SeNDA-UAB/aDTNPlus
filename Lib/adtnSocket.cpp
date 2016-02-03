c7bb616e50062e71b614fc8e8496eb71c5d3ef9a
Blackcatn13 <mdd_13@hotmail.com>
2016-02-03 16:30:49 +0100

    Adds first implementation of a library. The library for now only receives messages. Adds new basicReceiver using this library.

---
+++ Lib/adtnSocket.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2016 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE adtnSocket.cpp
+ * AUTHOR Blackcatn13
+ * DATE Feb 3, 2016
+ * VERSION 1
+ * This file contains the implementation of the adtnSocket
+ */
+
+#include "adtnSocket.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <sstream>
+
+adtnSocket::adtnSocket(std::string ip, int sendPort, int recvPort,
+                       std::string recvIp)
+    : m_nodeIp(ip),
+      m_listeningIp(recvIp),
+      m_sendPort(sendPort),
+      m_recvPort(recvPort),
+      m_nodeName("LIB"),
+      m_sendSocket(-1),
+      m_recvSocket(-1) {
+}
+
+adtnSocket::adtnSocket(std::string ip, int sendPort, int recvPort)
+    : adtnSocket(ip, sendPort, recvPort, "") {
+}
+
+adtnSocket::adtnSocket(std::string ip, int port, bool send)
+    : adtnSocket(ip, send ? port : -1, send ? -1 : port, "") {
+}
+
+adtnSocket::~adtnSocket() {
+  if (m_sendSocket != -1)
+    close(m_sendSocket);
+  if (m_recvSocket != -1)
+    close(m_recvSocket);
+}
+
+void adtnSocket::connect(int appId) {
+  std::string ip = m_listeningIp == "" ? m_nodeIp : m_listeningIp;
+  sockaddr_in remoteAddr = { 0 };
+  remoteAddr.sin_family = AF_INET;
+  remoteAddr.sin_port = htons(m_recvPort);
+  remoteAddr.sin_addr.s_addr = inet_addr(ip.c_str());
+  m_recvSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (m_recvSocket == -1) {
+    std::stringstream ss;
+    ss << "Cannot create socket, reason: " << strerror(errno);
+    throw(ss.str());
+  } else {
+    if (::connect(m_recvSocket, reinterpret_cast<sockaddr*>(&remoteAddr),
+                  sizeof(remoteAddr)) < 0) {
+      std::stringstream ss;
+          ss << "Cannot connect with node, reason: " << strerror(errno);
+          throw(ss.str());
+    } else {
+      uint8_t type = htons(0);
+      uint32_t appIdn = htonl(appId);
+      int writed = ::send(m_recvSocket, &type, sizeof(type), 0);
+      if (writed < 0) {
+        std::stringstream ss;
+            ss << "Cannot write to socket, reason: " << strerror(errno);
+            throw(ss.str());
+      } else {
+        writed = ::send(m_recvSocket, &appIdn, sizeof(appIdn), 0);
+        if (writed < 0) {
+          std::stringstream ss;
+              ss << "Cannot write to socket, reason: " << strerror(errno);
+              throw(ss.str());
+        }
+      }
+    }
+  }
+}
+
+std::string adtnSocket::recv() {
+  int payloadSize = 0;
+  int receivedSize = ::recv(m_recvSocket, &payloadSize, sizeof(payloadSize), 0);
+  char* payloadraw = new char[payloadSize];
+  int receivedLength = 0;
+  while (receivedLength != payloadSize) {
+    receivedSize = ::recv(m_recvSocket, payloadraw + receivedLength,
+                          payloadSize - receivedLength, 0);
+    receivedLength += receivedSize;
+  }
+  std::string payload = std::string(payloadraw, payloadSize);
+  return payload;
+}
+
+void adtnSocket::send(std::string destination, std::string message) {
+}
---
+++ Lib/adtnSocket.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE adtnSocket.h
+ * AUTHOR Blackcatn13
+ * DATE Feb 3, 2016
+ * VERSION 1
+ * This file contains the definition of the adtnSocket.
+ */
+#ifndef LIB_ADTNSOCKET_H_
+#define LIB_ADTNSOCKET_H_
+
+#include <string>
+#include <stdexcept>
+
+class adtnSocketException : public std::runtime_error {
+ public:
+  explicit adtnSocketException(const std::string &what)
+      : runtime_error(what) {
+  }
+};
+
+class adtnSocket {
+ public:
+  adtnSocket(std::string ip, int sendPort, int recvPort,
+             std::string recvIp);
+  adtnSocket(std::string ip, int sendPort, int recvPort);
+  adtnSocket(std::string ip, int port, bool send);
+  virtual ~adtnSocket();
+  void connect(int appId);
+  std::string recv();
+  void send(std::string destination, std::string message);
+ private:
+  std::string m_nodeIp;
+  std::string m_listeningIp;
+  int m_sendPort;
+  int m_recvPort;
+  std::string m_nodeName;
+  int m_sendSocket;
+  int m_recvSocket;
+};
+
+#endif  // LIB_ADTNSOCKET_H_
