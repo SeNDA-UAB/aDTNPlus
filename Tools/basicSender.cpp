88f54824778b5e67554f9b868456029ddd9a7d52
Blackcatn13 <mdd_13@hotmail.com>
2016-02-04 09:39:08 +0100

    Changed the old basicSender and basicReceiver for the ones using the library.

--- Tools/basicSender.cpp
+++ Tools/basicSender.cpp
@@ -30,7 +30,7 @@
 #include <string>
 #include <iostream>
 #include <cstring>
-#include "Bundle/Bundle.h"
+#include "adtnSocket.h"
 
 static void help(std::string program_name) {
   std::cout << program_name << " is part of the SeNDA aDTNPlus platform\n"
@@ -40,7 +40,6 @@ static void help(std::string program_name) {
             << "Required options:\n"
             << "   [-i | --listeningIP] Ip\t\t\tIP of the listening node.\n"
             << "   [-p | --port] port\t\t\tPort where the node is listening.\n"
-            << "   [-s | --source] source name\t\t\tWhich node is the source.\n"
             << "   [-d | --destination] destination node\t\t\t"
             << "Which node is the destination.\n"
             << "   [-m | --message] message to send\t\t\tThe message to send.\n"
@@ -53,21 +52,15 @@ int main(int argc, char **argv) {
   int opt = -1, option_index = 0;
   std::string ip = "";
   int port = -1;
-  std::string source = "";
   std::string destination = "";
   std::string message = "";
 
-  static struct option long_options[] = {
-      { "listeningIP", required_argument, 0, 'i' },
-      { "port", required_argument, 0, 'p' },
-      { "source", required_argument, 0, 's' },
-      { "destination", required_argument, 0, 'd' },
-      {"message", required_argument, 0, 'm' },
-      { "help", no_argument, 0, 'h' },
-      {0, 0, 0, 0 }
-  };
+  static struct option long_options[] = { { "listeningIP", required_argument, 0,
+      'i' }, { "port", required_argument, 0, 'p' }, { "destination",
+      required_argument, 0, 'd' }, { "message", required_argument, 0, 'm' }, {
+      "help", no_argument, 0, 'h' }, { 0, 0, 0, 0 } };
 
-  while ((opt = getopt_long(argc, argv, "i:p:s:d:m:h", long_options,
+  while ((opt = getopt_long(argc, argv, "i:p:d:m:h", long_options,
                             &option_index))) {
     switch (opt) {
       case 'i':
@@ -75,9 +68,6 @@ int main(int argc, char **argv) {
         break;
       case 'p':
         port = std::atoi(optarg);
-        break;
-      case 's':
-        source = std::string(optarg);
         break;
       case 'd':
         destination = std::string(optarg);
@@ -94,36 +84,14 @@ int main(int argc, char **argv) {
     if (opt == -1)
       break;
   }
-  if (ip == "" || port == -1 || source == "" || destination == ""
-      || message == "") {
+  if (ip == "" || port == -1 || destination == "" || message == "") {
     help(std::string(argv[0]));
     exit(0);
   }
+
+  adtnSocket s = adtnSocket(ip, port);
   try {
-    Bundle b = Bundle(source, destination, message);
-    sockaddr_in remoteAddr = { 0 };
-    remoteAddr.sin_family = AF_INET;
-    remoteAddr.sin_port = htons(port);
-    remoteAddr.sin_addr.s_addr = inet_addr(ip.c_str());
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-      std::cout << "Cannot create socket, reason: " << strerror(errno)
-                << std::endl;
-    } else {
-      if (connect(sock, reinterpret_cast<sockaddr*>(&remoteAddr),
-                  sizeof(remoteAddr)) < 0) {
-        std::cout << "Cannot connect with node, reason: " << strerror(errno)
-                  << std::endl;
-      } else {
-        send(sock, source.c_str(), 1024, 0);
-        std::string bundleRaw = b.toRaw();
-        uint32_t bundleLength = bundleRaw.length();
-        uint32_t nBundleLength = htonl(bundleLength);
-        send(sock, &nBundleLength, sizeof(nBundleLength), 0);
-        send(sock, bundleRaw.c_str(), bundleLength, 0);
-      }
-      close(sock);
-    }
+    s.send(destination, message);
   } catch (const std::exception &e) {
     std::cout << e.what() << std::endl;
   }
