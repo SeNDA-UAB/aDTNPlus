c4fe51be0c7a7d22bea9976088ff674fb67bf2a5
Blackcatn13 <mdd_13@hotmail.com>
2016-04-04 17:09:06 +0200

    Adds FirstADTNPlusFwk processor. Adds new default functions to the nodeState.

---
+++ BundleAgent/Node/BundleProcessor/FirstADTNPlusFwk.cpp
@@ -0,0 +1,291 @@
+/*
+ * Copyright (c) 2016 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE Framework1Processor.cpp
+ * AUTHOR Blackcatn13
+ * DATE Apr 4, 2016
+ * VERSION 1
+ *
+ */
+
+#include "Node/BundleProcessor/FirstADTNPlusFwk.h"
+#include <memory>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "Node/BundleProcessor/BundleProcessor.h"
+#include "Node/BundleQueue/BundleQueue.h"
+#include "Node/Neighbour/NeighbourTable.h"
+#include "Node/Config.h"
+#include "Node/BundleQueue/BundleContainer.h"
+#include "Bundle/Bundle.h"
+#include "Bundle/PrimaryBlock.h"
+#include "Utils/Logger.h"
+#include "Node/AppListener/ListeningAppsTable.h"
+#include "Node/BundleProcessor/PluginAPI.h"
+#include "Utils/globals.h"
+
+NEW_PLUGIN(FirstADTNPlusFwk, "First active DTN framework", "1.0",
+           "This processor allows to implement up to 5 functions.")
+
+const std::string FirstADTNPlusFwk::m_header = "#include <vector>\n"
+    "#include <string>\n"
+    "#include <algorithm>\n"
+    "#include <cstdlib>\n"
+    "#include \"Bundle/BundleInfo.h\"\n"
+    "#include \"adtnPlus/Json.h\"\n"
+    "#include \"adtnPlus/Worker.h\"\n"
+    "extern \"C\" {\n"
+    "const uint64_t g_timeFrom2000 = 946684800;\n";
+const std::string FirstADTNPlusFwk::m_bigSignature =
+    "%s f(Json ns, nlohmann::json bs, nlohmann::json bps, BundleInfo bi,"
+        " Worker<%s, Json, nlohmann::json, BundleInfo>> worker) {\n"
+        "auto super[worker, ns, bps, bi]() {try{\n"
+        "worker.execute(ns, bps, bi);\n"
+        "return worker.getResult();\n"
+        "} catch (const WorkerException &e) {\n"
+        "throw e;\n"
+        "};";
+const std::string FirstADTNPlusFwk::m_littleSignature =
+    "%s f(Json ns, nlohmann::json bps, BundleInfo bi) {\n";
+const std::string FirstADTNPlusFwk::m_footer = "}}";
+const std::string FirstADTNPlusFwk::m_commandLine =
+    "g++ -w -fPIC -shared -std=c++11 %s -o %s 2>&1";
+
+FirstADTNPlusFwk::FirstADTNPlusFwk()
+    : m_voidWorker(m_header + stringFormat(m_bigSignature, "void", "void"),
+                   m_footer, "f", m_commandLine, "./"),
+      m_boolWorker(m_header + stringFormat(m_bigSignature, "bool", "bool"),
+                   m_footer, "f", m_commandLine, "./"),
+      m_vectorWorker(
+          m_header
+              + stringFormat(m_bigSignature, "std::vector<std::string>",
+                             "std::vector<std::string>"),
+          m_footer, "f", m_commandLine, "./"),
+      m_ext1DefaultWorker(m_header + stringFormat(m_littleSignature, "void"),
+                          m_footer, "f", m_commandLine, "./"),
+      m_ext2DefaultWorker(m_header + stringFormat(m_littleSignature, "void"),
+                          m_footer, "f", m_commandLine, "./"),
+      m_ext3DefaultWorker(m_header + stringFormat(m_littleSignature, "bool"),
+                          m_footer, "f", m_commandLine, "./"),
+      m_ext4DefaultWorker(m_header + stringFormat(m_littleSignature, "bool"),
+                          m_footer, "f", m_commandLine, "./"),
+      m_ext5DefaultWorker(
+          m_header
+              + stringFormat(m_littleSignature, "std::vector<std::string>"),
+          m_footer, "f", m_commandLine, "./") {
+}
+
+FirstADTNPlusFwk::~FirstADTNPlusFwk() {
+  if (!m_nodeState.empty()) {
+    std::ofstream nodeState(m_config.getNodeStatePath());
+    nodeState << m_nodeState.dump(2);
+    nodeState.close();
+  }
+}
+
+void FirstADTNPlusFwk::start(
+    Config config, std::shared_ptr<BundleQueue> bundleQueue,
+    std::shared_ptr<NeighbourTable> neighbourTable,
+    std::shared_ptr<ListeningAppsTable> listeningAppsTable) {
+  BundleProcessor::start(config, bundleQueue, neighbourTable,
+                         listeningAppsTable);
+  std::ifstream nodeState(m_config.getNodeStatePath());
+  m_nodeState.start(m_neighbourTable);
+  m_voidWorker.setPath(m_config.getCodesPath());
+  m_boolWorker.setPath(m_config.getCodesPath());
+  m_vectorWorker.setPath(m_config.getCodesPath());
+  m_ext1DefaultWorker.setPath(m_config.getCodesPath());
+  m_ext2DefaultWorker.setPath(m_config.getCodesPath());
+  m_ext3DefaultWorker.setPath(m_config.getCodesPath());
+  m_ext4DefaultWorker.setPath(m_config.getCodesPath());
+  m_ext5DefaultWorker.setPath(m_config.getCodesPath());
+  if (nodeState) {
+    try {
+      nodeState >> m_nodeState;
+      m_oldNodeState = m_nodeState;
+      nodeState.close();
+      std::string defaultForwardingCode =
+          m_nodeState["configuration"]["defaultCodes"]["forwarding"];
+      std::string defaultLifeCode =
+          m_nodeState["configuration"]["defaultCodes"]["lifetime"];
+      std::string defaultDestinationCode =
+          m_nodeState["configuration"]["defaultCodes"]["destination"];
+      std::string defaultBundleCreation =
+                m_nodeState["configuration"]["defaultCodes"]["creation"];
+      std::string defaultBundleDeletion =
+                m_nodeState["configuration"]["defaultCodes"]["deletion"];
+      try {
+        m_ext5DefaultWorker.generateFunction(defaultForwardingCode);
+        m_ext4DefaultWorker.generateFunction(defaultLifeCode);
+        m_ext3DefaultWorker.generateFunction(defaultDestinationCode);
+        m_ext2DefaultWorker.generateFunction(defaultBundleCreation);
+        m_ext1DefaultWorker.generateFunction(defaultBundleDeletion);
+      } catch (const WorkerException &e) {
+        LOG(11) << "Cannot create code worker, reason: " << e.what();
+      }
+    } catch (const std::invalid_argument &e) {
+      LOG(1) << "Error in NodeState json: " << e.what();
+      g_stop = true;
+    }
+  } else {
+    LOG(11) << "Cannot open the file " << m_config.getNodeStatePath();
+  }
+}
+
+void FirstADTNPlusFwk::processBundle(
+    std::unique_ptr<BundleContainer> bundleContainer) {
+  LOG(51) << "Processing a bundle container.";
+  LOG(55) << "Checking destination node.";
+  if (checkDestination(*bundleContainer)) {
+    LOG(55) << "We are the destination node.";
+    LOG(55) << "Checking destination app listening.";
+    std::vector<std::string> destinations = checkDispatch(*bundleContainer);
+    if (destinations.size() > 0) {
+      LOG(55) << "There is a listening app, dispatching the bundle.";
+      try {
+        dispatch(bundleContainer->getBundle(), destinations);
+        discard(std::move(bundleContainer));
+      } catch (const TableException &e) {
+        LOG(55) << "Restoring not dispatched bundle.";
+        restore(std::move(bundleContainer));
+      }
+    } else {
+      LOG(55) << "No listening app, restoring the bundle.";
+      restore(std::move(bundleContainer));
+    }
+  } else {
+    LOG(55) << "We are not the destination node.";
+    LOG(55) << "Checking lifetime.";
+    if (checkLifetime(*bundleContainer)) {
+      LOG(55) << "Bundle expired, discarding it.";
+      discard(std::move(bundleContainer));
+    } else {
+      LOG(55) << "Bundle is not expired yet.";
+      LOG(55) << "Checking neighbours. " << "";
+      std::vector<std::string> neighbours = checkForward(*bundleContainer);
+      if (neighbours.size() > 0) {
+        LOG(55) << "There are some neighbours.";
+        LOG(55) << "Checking if one of them is the bundle destination.";
+        auto it = std::find(
+            neighbours.begin(),
+            neighbours.end(),
+            bundleContainer->getBundle().getPrimaryBlock()->getDestination()
+                .substr(
+                0,
+                bundleContainer->getBundle().getPrimaryBlock()->getDestination()
+                    .find(":")));
+        if (it != neighbours.end()) {
+          LOG(55) << "Destination found, sending the bundle to it.";
+          std::vector<std::string> nextHop = std::vector<std::string>();
+          nextHop.push_back(*it);
+          try {
+            forward(bundleContainer->getBundle(), nextHop);
+            LOG(55) << "Discarding the bundle.";
+            discard(std::move(bundleContainer));
+          } catch (const ForwardException &e) {
+            LOG(1) << e.what();
+            LOG(55) << "The bundle has not been send, restoring the bundle.";
+            restore(std::move(bundleContainer));
+          }
+        } else {
+          LOG(55) << "Destination not found, "
+                  << "sending the bundle to all the neighbours.";
+          try {
+            forward(bundleContainer->getBundle(), neighbours);
+            LOG(55) << "Discarding the bundle.";
+            discard(std::move(bundleContainer));
+          } catch (const ForwardException &e) {
+            LOG(1) << e.what();
+            LOG(55) << "The bundle has not been send, restoring the bundle.";
+            restore(std::move(bundleContainer));
+          }
+        }
+      } else {
+        LOG(55) << "No neighbours found, restoring the bundle.";
+        restore(std::move(bundleContainer));
+      }
+    }
+  }
+  checkNodeStateChanges();
+}
+
+std::unique_ptr<BundleContainer> FirstADTNPlusFwk::createBundleContainer(
+    std::unique_ptr<Bundle> bundle) {
+  return std::unique_ptr<BundleContainer>(
+      new BundleContainer(std::move(bundle)));
+}
+
+bool FirstADTNPlusFwk::checkDestination(BundleContainer &bundleContainer) {
+  return false;
+}
+
+std::vector<std::string> FirstADTNPlusFwk::checkDispatch(
+    BundleContainer &bundleContainer) {
+  std::string destination = bundleContainer.getBundle().getPrimaryBlock()
+      ->getDestination();
+  std::string appId = destination.substr(destination.find(":") + 1);
+  std::vector<std::string> dispatch;
+  dispatch.push_back(appId);
+  return dispatch;
+}
+
+std::vector<std::string> FirstADTNPlusFwk::checkForward(
+    BundleContainer &bundleContainer) {
+  return std::vector<std::string>();
+}
+
+bool FirstADTNPlusFwk::checkLifetime(BundleContainer &bundleContainer) {
+  return false;
+}
+
+void FirstADTNPlusFwk::checkNodeStateChanges() {
+  if (m_nodeState["state"]["changed"]) {
+    m_nodeState["state"]["changed"] = false;
+    // Check what changed and act accordingly
+    if (m_nodeState["state"]["stop"]) {
+      m_nodeState["state"]["stop"] = false;
+      g_stop = true;
+    }
+    if (m_nodeState["configuration"]["logLevel"]
+        != m_oldNodeState["configuration"]["logLevel"]) {
+      Logger::getInstance()->setLogLevel(
+          m_nodeState["configuration"]["logLevel"]);
+    }
+    std::string code =
+        m_nodeState["configuration"]["defaultCodes"]["forwarding"];
+    if (code.compare(
+        m_oldNodeState["configuration"]["defaultCodes"]["forwarding"]) != 0) {
+      try {
+        // m_forwardWorker.generateFunction(code);
+      } catch (const WorkerException &e) {
+        LOG(11) << "Cannot create forward code worker, reason: " << e.what();
+      }
+    }
+    code = m_nodeState["configuration"]["defaultCodes"]["lifetime"];
+    if (code.compare(
+        m_oldNodeState["configuration"]["defaultCodes"]["lifetime"]) != 0) {
+      try {
+        // m_lifeWorker.generateFunction(code);
+      } catch (const WorkerException &e) {
+        LOG(11) << "Cannot create life code worker, reason: " << e.what();
+      }
+    }
+    m_oldNodeState = m_nodeState;
+  }
+}
+
---
+++ BundleAgent/Node/BundleProcessor/FirstADTNPlusFwk.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2016 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE Framework1Processor.h
+ * AUTHOR Blackcatn13
+ * DATE Apr 4, 2016
+ * VERSION 1
+ *
+ */
+#ifndef BUNDLEAGENT_NODE_BUNDLEPROCESSOR_FIRSTADTNPLUSFWK_H_
+#define BUNDLEAGENT_NODE_BUNDLEPROCESSOR_FIRSTADTNPLUSFWK_H_
+
+#include <memory>
+#include <vector>
+#include <string>
+#include "Utils/Json.h"
+#include "Node/BundleProcessor/BundleProcessor.h"
+#include "Node/Executor/Worker.h"
+#include "ExternTools/json/json.hpp"
+
+class Bundle;
+class BundleQueue;
+class BundleContainer;
+class NeighbourTable;
+class ListeningAppsTable;
+class BundleInfo;
+
+class FirstADTNPlusFwk : public BundleProcessor {
+ public:
+  FirstADTNPlusFwk();
+  virtual ~FirstADTNPlusFwk();
+
+  virtual void start(Config config, std::shared_ptr<BundleQueue> bundleQueue,
+                     std::shared_ptr<NeighbourTable> neighbourTable,
+                     std::shared_ptr<ListeningAppsTable> listeningAppsTable);
+
+ private:
+  virtual void processBundle(std::unique_ptr<BundleContainer> bundleContainer);
+
+  virtual std::unique_ptr<BundleContainer> createBundleContainer(
+      std::unique_ptr<Bundle> Bundle);
+
+  std::vector<std::string> checkDispatch(BundleContainer &bundleContainer);
+
+  virtual bool checkLifetime(BundleContainer &bundleContainer);
+
+  virtual std::vector<std::string> checkForward(
+      BundleContainer &bundleContainer);
+
+  virtual bool checkDestination(BundleContainer &bundleContainer);
+
+  virtual void checkNodeStateChanges();
+
+  Worker<void, Json, nlohmann::json, nlohmann::json, BundleInfo,
+      Worker<void, Json, nlohmann::json, BundleInfo>> m_voidWorker;
+
+  Worker<bool, Json, nlohmann::json, nlohmann::json, BundleInfo,
+      Worker<bool, Json, nlohmann::json, BundleInfo>> m_boolWorker;
+
+  Worker<std::vector<std::string>, Json, nlohmann::json, nlohmann::json,
+      BundleInfo,
+      Worker<std::vector<std::string>, Json, nlohmann::json, BundleInfo>> m_vectorWorker;
+
+  Worker<void, Json, nlohmann::json, BundleInfo> m_ext1DefaultWorker;
+
+  Worker<void, Json, nlohmann::json, BundleInfo> m_ext2DefaultWorker;
+
+  Worker<bool, Json, nlohmann::json, BundleInfo> m_ext3DefaultWorker;
+
+  Worker<bool, Json, nlohmann::json, BundleInfo> m_ext4DefaultWorker;
+
+  Worker<std::vector<std::string>, Json, nlohmann::json, BundleInfo> m_ext5DefaultWorker;
+  /**
+   * Variable that holds the parameters used in the processor calls.
+   */
+  Json m_nodeState;
+  /**
+   * Variable that holds the old parameters to check what changed.
+   */
+  Json m_oldNodeState;
+
+  static const std::string m_header;
+  static const std::string m_bigSignature;
+  static const std::string m_littleSignature;
+  static const std::string m_footer;
+  static const std::string m_commandLine;
+};
+
+#endif  // BUNDLEAGENT_NODE_BUNDLEPROCESSOR_FIRSTADTNPLUSFWK_H_
