336e5a17aafc91a9fd55394fdc706d3fcadb11df
Blackcatn13 <mdd_13@hotmail.com>
2016-04-19 10:31:05 +0200

    Fixes and updates the NeighbourDiscovery Tests.

--- Test/BundleAgent/Node/Neighbour/NeighbourDiscoveryTest.cpp
+++ Test/BundleAgent/Node/Neighbour/NeighbourDiscoveryTest.cpp
@@ -26,12 +26,15 @@
 #include <string>
 #include <fstream>
 #include <memory>
+#include <vector>
 #include "Node/Neighbour/NeighbourDiscovery.h"
 #include "gtest/gtest.h"
 #include "Node/Neighbour/NeighbourTable.h"
 #include "Node/Neighbour/Neighbour.h"
 #include "Node/Config.h"
 #include "Utils/globals.h"
+#include "Node/EndpointListener/ListeningEndpointsTable.h"
+#include "Node/EndpointListener/Endpoint.h"
 
 /**
  * Check NeighbourCleaner thread.
@@ -39,7 +42,7 @@
  * cleaner delete them.
  * Stop the cleaner and check that it has really stopped.
  */
-/*TEST(NeighbourDiscoveryTest, NeighbourCleanerTest) {
+TEST(NeighbourDiscoveryTest, NeighbourCleanerTest) {
   g_stop = false;
   std::ofstream ss;
   ss.open("adtn.ini");
@@ -58,11 +61,17 @@
   Config cf = Config("adtn.ini");
   std::shared_ptr<NeighbourTable> nt = std::shared_ptr<NeighbourTable>(
       new NeighbourTable());
-  NeighbourDiscovery nd(cf, nt);
-  nt->update(std::make_shared<Neighbour>("node100", "192.168.1.1", 4000, std::vector<std::string>()));
+  std::shared_ptr<ListeningEndpointsTable> let = std::shared_ptr<
+      ListeningEndpointsTable>(new ListeningEndpointsTable());
+  NeighbourDiscovery nd(cf, nt, let);
+  nt->update(
+      std::make_shared<Neighbour>("node100", "192.168.1.1", 4000,
+                                  std::vector<std::string>( { "e1" })));
   auto neighbours = nt->getValues();
   ASSERT_EQ(static_cast<size_t>(1), neighbours.size());
-  nt->update(std::make_shared<Neighbour>("node101", "192.168.1.1", 4000, std::vector<std::string>()));
+  nt->update(
+      std::make_shared<Neighbour>("node101", "192.168.1.1", 4000,
+                                  std::vector<std::string>( { "e2" })));
   neighbours.clear();
   neighbours = nt->getValues();
   ASSERT_EQ(static_cast<size_t>(2), neighbours.size());
@@ -70,7 +79,9 @@
   neighbours.clear();
   neighbours = nt->getValues();
   ASSERT_EQ(static_cast<size_t>(2), neighbours.size());
-  nt->update(std::make_shared<Neighbour>("node101", "192.168.1.1", 4000, std::vector<std::string>()));
+  nt->update(
+      std::make_shared<Neighbour>("node101", "192.168.1.1", 4000,
+                                  std::vector<std::string>( { "e2" })));
   sleep(2);
   neighbours.clear();
   neighbours = nt->getValues();
@@ -83,19 +94,21 @@
   g_stop = true;
   // The neighbour cleaner thread has been stopped, so the new neighbours
   // must not be cleaned.
-  nt->update(std::make_shared<Neighbour>("node101", "192.168.1.1", 4000, std::vector<std::string>()));
+  nt->update(
+      std::make_shared<Neighbour>("node101", "192.168.1.1", 4000,
+                                  std::vector<std::string>( { "e2" })));
   sleep(5);
   neighbours.clear();
   neighbours = nt->getValues();
   ASSERT_EQ(static_cast<size_t>(1), neighbours.size());
-}*/
+}
 
 /**
  * Check if the sender and receiver threads are working correctly.
  * To do this, we start the neighbour discovery, and put in test mode.
  * With this we are going to have a neighbour, ourselves.
  */
-/*TEST(NeighbourDiscoveryTest, NeighbourSendAndReceiveTest) {
+TEST(NeighbourDiscoveryTest, NeighbourSendAndReceiveTest) {
   g_stop = false;
   std::ofstream ss;
   ss.open("adtn1.ini");
@@ -117,14 +130,24 @@
   sleep(1);
   std::shared_ptr<NeighbourTable> nt = std::shared_ptr<NeighbourTable>(
       new NeighbourTable());
-  NeighbourDiscovery nd(cf, nt);
+  std::shared_ptr<ListeningEndpointsTable> let = std::shared_ptr<
+      ListeningEndpointsTable>(new ListeningEndpointsTable());
+  NeighbourDiscovery nd(cf, nt, let);
   sleep(3);
-  auto neighbours = nt->getValues();
-  ASSERT_EQ(static_cast<size_t>(1), neighbours.size());
-  ASSERT_EQ("node1", nt->getValue(*neighbours.begin())->getId());
-  ASSERT_EQ("127.0.0.1", nt->getValue(*neighbours.begin())->getNodeAddress());
-  ASSERT_EQ(static_cast<uint16_t>(40000),
-            nt->getValue(*neighbours.begin())->getNodePort());
+  ASSERT_NO_THROW(nt->getValue("node1"));
+  auto neighbour = nt->getValue("node1");
+  ASSERT_EQ("node1", neighbour->getId());
+  ASSERT_EQ("127.0.0.1", neighbour->getNodeAddress());
+  ASSERT_EQ(static_cast<uint16_t>(40000), neighbour->getNodePort());
+  // Add an endpoint, an check if its in the neighbour table.
+  let->update("This", Endpoint("This", "127.0.0.1", 50, 2));
+  sleep(3);
+  ASSERT_NO_THROW(nt->getValue("node1"));
+  neighbour = nt->getValue("node1");
+  ASSERT_EQ("node1", neighbour->getId());
+  ASSERT_EQ("127.0.0.1", neighbour->getNodeAddress());
+  ASSERT_EQ(static_cast<uint16_t>(40000), neighbour->getNodePort());
+  ASSERT_EQ(std::vector<std::string>({"This"}), neighbour->getEndpoints());
   g_stop = true;
   sleep(5);
-}*/
+}
