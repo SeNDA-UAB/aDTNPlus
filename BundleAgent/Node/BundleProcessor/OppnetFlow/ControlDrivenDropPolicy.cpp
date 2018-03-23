dec703c33a45e99396a357e64b31363411899969
MCarmen <detoro.mcarmen@gmail.com>
2018-03-23 10:31:39 +0100

    Added the classes to implement the Drop Policy

---
+++ BundleAgent/Node/BundleProcessor/OppnetFlow/ControlDrivenDropPolicy.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2018 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE ControlFavorDropPolicy.cpp
+ * AUTHOR MCarmen
+ * DATE Mar 20, 2018
+ * VERSION 1
+ *
+ */
+
+#include <cstdint>
+#include "Bundle/BundleInfo.h"
+#include "Node/BundleProcessor/OppnetFlow/ControlDrivenDropPolicy.h"
+
+
+
+
+
+ControlDrivenDropPolicy::ControlDrivenDropPolicy(
+    std::string currentNodeId, NodeStateJson& nodeStateForPriorization) :
+    m_currentNodeId(currentNodeId) {
+
+   const std::string configurationNSEntries[] {"myCtrlDirective", "myCtrlMetric",
+     "forwardedCtrlDirective", "forwardedMetricDirective"};
+   uint8_t i = 0;
+
+   for(auto entry : configurationNSEntries){
+     m_ctrlPriorizationConfiguration[i++] =
+         (static_cast<bool>(nodeStateForPriorization[entry])) ? 1 : 0;
+   }
+}
+
+ControlDrivenDropPolicy::~ControlDrivenDropPolicy() {
+}
+
+bool ControlDrivenDropPolicy::operator ()(const BundleInfo& bundle1,
+                                         const BundleInfo& bundle2) const {
+  uint8_t bundle1Score = calculatePriorityScore(bundle1);
+  uint8_t bundle2Score = calculatePriorityScore(bundle2);
+  if (bundle1Score == bundle2Score){
+    bundle1Score +=
+        ((bundle1.getCreationTimestamp() < bundle2.getCreationTimestamp()) ?
+            -1 : 1);
+  }
+  return (bundle1Score < bundle2Score);
+}
+
+const uint8_t ControlDrivenDropPolicy::calculatePriorityScore(
+    const BundleInfo& bundleI) const {
+  uint8_t score;
+  bool includesMEB = false;
+  bool isMyBundle = (bundleI.getSource() == m_currentNodeId);
+
+  if (isMyBundle) {
+    score += ControlDrivenDropPolicy::I_AM_SRC_NODE_SCORE;
+  }
+  if (bundleI.hasMetadataTypeBlock(
+      static_cast<uint8_t>(MetadataTypes::CONTROL_DIRECTIVE_MEB))) {
+    includesMEB = true;
+
+    uint8_t priorizationFlag = (isMyBundle) ?
+        m_ctrlPriorizationConfiguration[static_cast<uint8_t>(CtrlPriorization::MY_CTRL_DIRECTIVE)] :
+        m_ctrlPriorizationConfiguration[static_cast<uint8_t>(CtrlPriorization::FORWARDED_CTRL_DIRECTIVE)];
+    score += (ControlDrivenDropPolicy::CTRL_DIRECTIVE_MEB_SCORE * priorizationFlag);
+  }
+  //not elseif to support piggybacking
+  if (bundleI.hasMetadataTypeBlock(
+      static_cast<uint8_t>(MetadataTypes::CONTROL_METRICS_MEB))) {
+    includesMEB = true;
+    uint8_t priorizationFlag = (isMyBundle) ?
+            m_ctrlPriorizationConfiguration[static_cast<uint8_t>(CtrlPriorization::MY_CTRL_METRIC)] :
+            m_ctrlPriorizationConfiguration[static_cast<uint8_t>(CtrlPriorization::FORWARDED_CTRL_METRIC)];
+    score += (ControlDrivenDropPolicy::CTRL_METRIC_MEB_SCORE * priorizationFlag);
+
+  }
+  if (!includesMEB) {
+    score += ControlDrivenDropPolicy::DATA_PAYLOAD_SCORE;
+  }
+
+  return score;
+}
---
+++ BundleAgent/Node/BundleProcessor/OppnetFlow/ControlDrivenDropPolicy.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2018 SeNDA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/**
+ * FILE ControlFavorDropPolicy.h
+ * AUTHOR MCarmen
+ * DATE Mar 20, 2018
+ * VERSION 1
+ *
+ */
+#ifndef BUNDLEAGENT_NODE_BUNDLEPROCESSOR_OPPNETFLOW_CONTROLDRIVENDROPPOLICY_H_
+#define BUNDLEAGENT_NODE_BUNDLEPROCESSOR_OPPNETFLOW_CONTROLDRIVENDROPPOLICY_H_
+
+#include <string>
+#include "Node/BundleProcessor/OppnetFlow/DropPolicy.h"
+#include "Node/JsonFacades/NodeStateJson.h"
+
+
+class ControlDrivenDropPolicy : public DropPolicy{
+ public:
+  ControlDrivenDropPolicy(std::string currentNodeId,
+                          NodeStateJson& nodeStateForPriorization);
+  virtual ~ControlDrivenDropPolicy();
+
+
+  /**
+   * Comparator function used to enqueue a bundle. If bundle1 should be
+   * discarded in pro of bundle2, the method has to return true.
+   * If the method returns true bundle1 will occupy a lesser position in the
+   * queue, i,e, closer to the 0 position. When a bundle has to be discarded
+   * the firsts elements in the queue are the ones to go.
+   * @param bundle1
+   * @param bundle2
+   * @return true if bundle1 should be discared in pro of bundle2.
+   */
+  bool operator()(const BundleInfo& bundle1, const BundleInfo& bundle2) const;
+
+ protected:
+   static const uint8_t I_AM_SRC_NODE_SCORE = 100;
+   static const uint8_t CTRL_DIRECTIVE_MEB_SCORE = 50;
+   static const uint8_t CTRL_METRIC_MEB_SCORE = 30;
+   static const uint8_t DATA_PAYLOAD_SCORE = 10;
+
+ private:
+  /**
+   * Identifier of the current node which is using the comparator
+   */
+  std::string m_currentNodeId;
+
+  /**
+   * Node state number of entries to, initially, define if the control
+   * directives, and control metrics bundles are prioritized over the
+   * data bundles.
+   */
+  static const uint8_t CTRL_PRIORIZATION_PARAMS = 4;
+
+  /**
+   * Node state entries values that define if control directives and control
+   * metrics bundles are prioritized over the data bundles.
+   */
+  static uint8_t m_ctrlPriorizationConfiguration[CTRL_PRIORIZATION_PARAMS];
+
+  /**
+   * Contains the position in the array ctrlPriorizationConfiguration of the
+   * configuration regarding to the control directives and metrics a node
+   * generates or has to forward.
+   */
+  enum class  CtrlPriorization {
+    MY_CTRL_DIRECTIVE,
+    MY_CTRL_METRIC,
+    FORWARDED_CTRL_DIRECTIVE,
+    FORWARDED_CTRL_METRIC,
+  };
+
+  /**
+   * Calculates the priority of the bundle based on an score system:
+   * If I am the sources -> 100
+   * If I have a CtrolDirectiveMEB -> 50
+   * If I have a CtrlMetricMEB -> 30
+   * If I have another MEB -> 10
+   *
+   * The controlMEB's are multiplied by 1 or 0 depending if they are prioritized
+   * over the data bundles or not. This priorization is specified in the node
+   * state conf file.
+   * In case of a tie the casting desition is given by the life time of
+   * the bundle. The oldest one has to go.
+   */
+  const uint8_t calculatePriorityScore(const BundleInfo &bundleI) const;
+
+};
+
+#endif  // BUNDLEAGENT_NODE_BUNDLEPROCESSOR_OPPNETFLOW_CONTROLDRIVENDROPPOLICY_H_
