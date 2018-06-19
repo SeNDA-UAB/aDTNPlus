b76678c48149122d863e1febf7fb890d98e1e3c6
MCarmen <detoro.mcarmen@gmail.com>
2018-06-19 15:57:19 +0200

    Consumer/producer approach almost finished

--- Tools/SDONController.cpp
+++ Tools/SDONController.cpp
@@ -22,15 +22,21 @@
  *
  */
 
+
+#include <adtnSocket.h>
+#include <chrono>
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <Utils/Logger.h>
 #include "SDONController.h"
 #include "Node/BundleProcessor/OppnetFlow/OppnetFlowBundleProcessor.h"
 #include "Bundle/BundleTypes.h"
 #include "Bundle/NumericMEB.h"
 #include "Bundle/ControlMetricsMEB.h"
-#include <adtnSocket.h>
-#include <Utils/Logger.h>
-#include <iostream>
-#include <memory>
+#include "Bundle/ControlDirectiveMEB.h"
+#include "Tools/controllerGlobals.h"
+
 
 
 SDONController::SDONController(std::string nodeIp, int nodePortToSend,
@@ -45,7 +51,7 @@ SDONController::SDONController(std::string nodeIp, int nodePortToSend,
       m_socket_to_send(adtnSocket(nodeIp, nodePortToSend)),
       m_socket_to_recv(adtnSocket(nodeIp, nodePortToRecv, false)) {
   m_socket_to_recv.connect(app_addr);
-
+  //launch producer thread.
 }
 
 SDONController::~SDONController() {
@@ -65,30 +71,152 @@ const std::map<NetworkMetricsControlCode, value_t> SDONController::recvControlMe
   //LOG(55) << "Controller: metrics received: " << payload << std::endl;
   std::cout << "Controller: metrics received: " << payload << std::endl;
   Bundle* bundle;
-    if ((bundle = m_socket_to_recv.getLastBundle()) != nullptr) {
-      //std::cout << "SDONController::recvControlMetrics::bundle received: " << bundle->toString() <<std::endl; //DEBUG
-      std::shared_ptr<ControlMetricsMEB> meb = std::static_pointer_cast<
-          ControlMetricsMEB>(
-          bundle->findMetadataExtensionBlock(MetadataTypes::CONTROL_METRICS_MEB));
-      if (meb != nullptr) {
-        metricsMap = meb->getFields();
-      } else {
-        throw NetworkMetricsNotFoundException();
-      }
+  if ((bundle = m_socket_to_recv.getLastBundle()) != nullptr) {
+    //std::cout << "SDONController::recvControlMetrics::bundle received: " << bundle->toString() <<std::endl; //DEBUG
+    std::shared_ptr<ControlMetricsMEB> meb = std::static_pointer_cast<
+        ControlMetricsMEB>(
+        bundle->findMetadataExtensionBlock(MetadataTypes::CONTROL_METRICS_MEB));
+    if (meb != nullptr) {
+      metricsMap = meb->getFields();
+    } else {
+      throw NetworkMetricsNotFoundException();
     }
+  }
 
   return metricsMap;
 }
 
+void SDONController::recvControlInfo(
+    std::vector<std::map<NetworkMetricsControlCode, value_t>> &metrics,
+    std::vector<std::map<DirectiveControlCode, value_t>> &directives) {
+  std::string payload = m_socket_to_recv.recv();
+  LOG(LOG_CONTROLLER) << "Controller: info received: " << payload << std::endl;
+  std::cout << "Controller: info received: " << payload << std::endl;
+  Bundle* bundle;
+  if ((bundle = m_socket_to_recv.getLastBundle()) != nullptr) {
+    //std::cout << "SDONController::recvControlMetrics::bundle received: " << bundle->toString() <<std::endl; //DEBUG
+    std::shared_ptr<ControlMetricsMEB> meb = std::static_pointer_cast<
+        ControlMetricsMEB>(
+        bundle->findMetadataExtensionBlock(MetadataTypes::CONTROL_METRICS_MEB));
+    if (meb != nullptr) {
+      metrics.push_back(meb->getFields());
+    } else {
+      std::shared_ptr<ControlDirectiveMEB> meb =
+          std::static_pointer_cast<ControlDirectiveMEB>(
+              bundle->findMetadataExtensionBlock(MetadataTypes::CONTROL_DIRECTIVE_MEB));
+          if ((meb != nullptr)){
+            directives.push_back(meb->getFields());
+          }else{
+            LOG(LOG_CONTROLLER) << "No control info received in window time" ;
+          }
+      }
+    }
+}
+
+void SDONController::launchControlMetricsProducer() {
+  LOG(LOG_CONTROLLER) <<
+  "Creating SDONController::metrics producer thread";
+
+  std::thread metricsProducer(
+      &SDONController::collectControlMetricsForAWindowTime(), this);
+  metricsProducer.detach();
+
+}
+
+//producer
 void SDONController::collectControlMetricsForAWindowTime() {
+  std::vector<std::map<NetworkMetricsControlCode, value_t>> metrics;
+  std::vector<std::map<DirectiveControlCode, value_t>> directives;
+  g_startedThread++;
+  while (!g_stop.load()) {
+    auto endWindowTime_s = std::chrono::steady_clock::now()
+        + std::chrono::seconds(m_recvWindowTime);
+
+    while (endWindowTime_s > std::chrono::steady_clock::now()) {
+      recvControlInfo(metrics, directives);
+      // falta que el recive bolquegi pel temps restant al timeout.
+    } //end window time
+
+    //start locking code section
+    {
+      std::lock_guard<std::mutex> lck(m_mutex);
+      m_receivedControlMetrics = metrics;
+      m_receivedControlDirectives = directives;
+    }
+    if(!m_receivedControlMetrics.empty()
+        || !m_receivedControlDirectives.empty()){
+      m_conditionVariableOverControlInfo.notify_all();
+    }
+
+  }
+  g_stopped++;
+
 }
 
-void SDONController::sendControlDirectives(
-    std::map<DirectiveControlCode, value_t> directives) {
+void SDONController::consumeControlMetrics() {
+  g_startedThread++; //??? aqui?
+  while (!g_stop.load()) {
+    std::unique_lock<std::mutex> lck(m_mutex);
+    m_conditionVariableOverControlInfo.wait(lck);
+    //at this point we are sure we can access to m_receivedMetrics
+    //and m_receivedDirectives and there one of them is not empty.
+    processControlMetrics();
+  }
+  g_stopped++;
 }
 
-void SDONController::processControlMetrics(
-    std::forward_list<std::map<NetworkMetricsControlCode, value_t>>& metricsList) {
+
+
+void SDONController::processControlMetrics() {
+  //to avoid data race over the metris/directives list.
+  //the producer maight be quiquer than the consumer to produce more
+  //data.
+  std::vector<std::map<NetworkMetricsControlCode, value_t>> metrics =
+      m_receivedControlMetrics;
+  std::vector<std::map<DirectiveControlCode, value_t>> directives =
+      m_receivedControlDirectives;
+  for(std::map<NetworkMetricsControlCode, value_t>metric : metrics){
+
+  }
+
+
 }
+//void SDONController::collectControlMetricsForAWindowTimeAndSendDirective() {
+///*
+// uint32_t sleepTime =
+//      m_controlParameters[ControlParameterCode::REPORT_FREQUENCY];
+//  g_startedThread++;
+//
+//  LOG(LOG_SEND_METRICS_THREAD) << "Creating reportingNetworkMetrics thread";
+//  while (!g_stop.load()) {
+//    std::this_thread::sleep_for(std::chrono::seconds(sleepTime));
+//    sendNetworkMetrics();
+//    m_networkMetrics.reset();
+//  }  //end while true loop
+//  LOG(LOG_SEND_METRICS_THREAD) << "Exit reportingNetworkMetrics thread.";
+//  g_stopped++;
+// */
+//  g_startedThread++;
+//  LOG(LOG_SENSING_METRICS_THREAD_START) << "Starting sensing metrics thread";
+//  while (!g_stop.load()) {
+//    std::this_thread::sleep_for(std::chrono::seconds());
+//
+//  } //end while true loop
+//  g_stopped++;
+//}
+//
+//void SDONController::sendControlDirectives(
+//    std::map<DirectiveControlCode, value_t> directives) {
+//}
+//
+//void SDONController::scheduleCollectControlMetricsAndSendDirective() {
+//  std::thread metricsCollectorAndDirectiveSender(&SDONController::collectControlMetricsForAWindowTimeAndSendDirective, this);
+//  metricsCollectorAndDirectiveSender.detach();
+//}
+//
+//void SDONController::processControlMetrics(
+//    std::forward_list<std::map<NetworkMetricsControlCode, value_t>>& metricsList) {
+//
+//}
 
 
